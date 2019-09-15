89229af91c9fde5c30277a858bdcb3d285758a0e
Caltrop <47452720+CaltropUwU@users.noreply.github.com>
2019-09-15 14:56:57 +0200

    Add files via upload

--- generator.js
+++ generator.js
@@ -26,6 +26,10 @@ colorCurrentTile.onchange = function() {currentTileColor = this.value};
 colorCurrentTileback.onchange = function() {backtrackColor = this.value};
 
 var grid = [];
+var done = false;
+
+var start;
+var end;
 
 var currentTile;
 var stack = [];
@@ -55,21 +59,27 @@ function resetRNG() {
 var seed = Date.now()
 changeSeed(seed);
 
+var lastSelected = {};
+
+var infoDiv = document.getElementById('info');
+
 class Tile {
 	constructor(x,y) {
 		this.x = Math.floor(x);
 		this.y = Math.floor(y);
 
 		this.visited = false;
+		this.closed = false;
 
 		this.gCost;
 		this.hCost;
 		this.fCost = this.gCost + this.hCost;
 
-		this.path = false;
+		this.parent = undefined;
 
-		this.edge = (this.x == 0 || this.x == width-1) || (this.y == 0 || this.y == height-1)
-		this.walls = [true, true, true, true];
+		this.edge = (this.x == 0 || this.x == width-1) || (this.y == 0 || this.y == height-1);
+
+		this.walls = [true, true, true, true]; //top left bottom right
 
 		this.createLine = function(x,y,x2,y2) {
 			ctx.beginPath();
@@ -94,6 +104,11 @@ class Tile {
 				ctx.fillStyle = visited;
 				ctx.fillRect(posX, posY, scale, scale);
 			};
+
+			if(this.selected) {
+				ctx.fillStyle = '#FF00FF55';
+				ctx.fillRect(posX, posY, scale, scale);
+			};
 		};
 
 		this.randomValidNeighbour = function() {
@@ -105,16 +120,69 @@ class Tile {
 			];
 			return neighbouring.sort((a,b) => 0.5 - random()).filter(t => t && !t.visited)[0];
 		};
+
 		this.visit = function() {
 			this.visited = true;
 			this.draw();
 		};
 		this.defineCosts = function(start, end) {
-			this.gCost = Math.abs((start.x - this.x) + (start.y - this.y));
-			this.hCost = Math.abs((end.x - this.x) + (end.y - this.y));
+			this.gCost = Math.abs(start.x - this.x) + Math.abs(start.y - this.y);
+			this.hCost = Math.abs(end.x - this.x) + Math.abs(end.y - this.y);
 			this.fCost = this.gCost + this.hCost;
 		};
+
+		this.mostCostEffectiveNeighbourWhichImGonnaUseToSolveThisMaze = function(end) {
+			let neighbouring = [];
+			
+			let top = grid.find(t => t.y == this.y-1 && t.x == this.x && !t.walls[2] && !t.closed);
+			let left = grid.find(t => t.y == this.y && t.x == this.x-1 && !t.walls[3] && !t.closed);
+			let bottom = grid.find(t => t.y == this.y+1 && t.x == this.x && !t.walls[0] && !t.closed);
+			let right = grid.find(t => t.y == this.y && t.x == this.x+1 && !t.walls[1] && !t.closed);
+
+			if(top) neighbouring.push(top);
+			if(left) neighbouring.push(left);
+			if(bottom) neighbouring.push(bottom);
+			if(right) neighbouring.push(right);
+			for(let i = 0; i < neighbouring.length; i++) {
+				neighbouring[i].defineCosts({x: this.x, y: this.y}, end);
+			};
+
+			neighbouring = neighbouring.sort((a,b) => a.fCost - b.fCost);
+
+			return neighbouring[0];
+		};
+
+		this.selected = false;
+
+		this.select = function() {
+			if(lastSelected) lastSelected.selected = false;
+			this.selected = true;
+			drawAll();
+
+			infoDiv.innerHTML = "<p>X: " + this.x + "</p>" +
+								"<p>Y: " + this.y + "</p>" +
+								"<p>Walls: " + JSON.stringify(this.walls) + "</p>" +
+								"<p>Edge: " + this.edge + "</p>" +
+								"<p>Visited: " + this.visited + "</p>" +
+								"<p>g/h/f - Cost: " + this.gCost + ", " + this.hCost + ", " + this.fCost + "</p>";
+
+			lastSelected = this;
+		};
 	};
+};
+
+
+
+canvas.onmousemove = function(e) {
+	let pointX = e.clientX - scale;
+	let pointY = e.clientY - (245 + scale);
+	let selected = grid.find(t => t.x <= pointX && t.x * scale >= pointX && t.y <= pointY && t.y * scale >= pointY);
+	if(!selected) {
+		if(lastSelected) lastSelected.selected = false;
+		drawAll(); 
+		return;
+	};
+	selected.select();
 };
 
 var heightSlider = document.getElementById('height');
@@ -125,6 +193,7 @@ var widthNumField = document.getElementById('widthNum');
 
 var mazeCheck = document.getElementById('autoMazeCheck');
 var genMazeButton = document.getElementById('Mazeify');
+var solveButton = document.getElementById('solve');
 
 var seedSlider = document.getElementById('seedSlider');
 var seedNumField = document.getElementById('seedNum');
@@ -149,6 +218,7 @@ widthSlider.oninput = function() {width = this.value; init(this.value, height);}
 
 mazeCheck.oninput = function() {autoMaze = this.checked; if(this.checked) init(width, height)}
 genMazeButton.onclick = function() {init(width, height, true)}
+solveButton.onclick = function() {alert("Do it yourself!")}
 
 seedSlider.oninput = function() {seed = this.value; changeSeed(seed); seedNumField.value = seed; init(width, height, false)};
 seedNumField.oninput = function() {seed = this.value; changeSeed(seed); seedSlider.value = seed; init(width, height, false)};
@@ -164,6 +234,7 @@ const drawAll = function() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for(let i = 0; i < grid.length; i++) {
 		grid[i].draw();
+		grid[i].defineCosts(start, end);
 	};
 };
 
@@ -222,7 +293,7 @@ function generateMazeStepByStep(start, end) {
 
 	clearInterval(intervalVar);
 
-	var drawStep = function(end, id) {
+	var drawStep = function(end, id, start) {
 		var next = currentTile.randomValidNeighbour();
 
 		if(next && id == opID) {
@@ -245,9 +316,55 @@ function generateMazeStepByStep(start, end) {
 			resetRNG();
 			if(loop) init(width, height, false);
 			end.walls = [false, false, false, false];
+			drawAll();
+			resetRNG();
 		};
 	};
-	intervalVar = setInterval(drawStep, delay, end, opID);
+	intervalVar = setInterval(drawStep, delay, end, opID, start);
+};
+
+function solveMaze(start, end) {
+	var intervalVar;
+	clearInterval(intervalVar);
+
+	currentTile = start;
+	var stack = [];
+
+	function drawSolve(start, end, id) {
+		
+		var next = currentTile.mostCostEffectiveNeighbourWhichImGonnaUseToSolveThisMaze(end);
+
+		console.log(stack.length)
+
+		if(currentTile.x == end.x && currentTile.y == end.y) {
+			console.log("DONE")
+			clearInterval(intervalVar);
+			drawAll();
+			resetRNG();
+		} else {
+			if(next && id == opID) {
+				currentTile = next;
+				stack.push(currentTile)
+				currentTile.closed = true;
+
+				drawAll();
+	
+				ctx.fillStyle = currentTileColor;
+				ctx.fillRect(currentTile.x * scale, currentTile.y * scale, scale, scale);
+	
+			} else if(stack.length && id == opID) {
+				currentTile = stack.pop();
+				ctx.fillStyle = backtrackColor;
+				ctx.fillRect(currentTile.x * scale, currentTile.y * scale, scale, scale);
+			} else {
+				console.log("FAILURE")
+				clearInterval(intervalVar);
+				drawAll();
+				resetRNG();
+			};
+		}
+	};
+	intervalVar = setInterval(drawSolve, delay, start, end, opID);
 };
 
 init(width, height);
@@ -273,12 +390,12 @@ function init(width,height, forced) {
 
 	if(autoMaze || forced) {
 		var edges = grid.filter(t => t.edge).sort((a,b) => 0.5 - random());
-		var start = edges[0];
+		start = edges[0];
 
 		var opX = ((start.x - Math.ceil((width-1) / 2)) * -1) + Math.ceil((width-1) / 2) -1
 		var opY = ((start.y - Math.ceil((height-1) / 2)) * -1) + Math.ceil((height-1) / 2) -1
 
-		var end = grid.find(t => t.x == opX && t.y == opY);
+		end = grid.find(t => t.x == opX && t.y == opY);
 
 
 		currentTile = start;
