9d0711c5f32f2ccbc8d74e5080071b0acad2c3bb
Caltrop <caltropuwu@gmail.com>
2019-09-19 18:54:47 +0200

    boids release

---
+++ boids/classes/cursor.js
@@ -0,0 +1,20 @@
+/*var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+class Cursor {
+    constructor() {
+        this.position = new Victor();
+        this.lastPosition = new Victor();
+
+        this.render = function() {
+
+        };
+    };
+};
+
+let cursor = new Cursor();
+
+canvas.onmousemove = function(e) {
+    cursor.lastPosition = cursor.position;
+    cursor.position = new Victor(e.clientX, e.clientY);
+};*/
